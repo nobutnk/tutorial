891adff2f218d28f6bbe9b4f4a734547de1b5fb8
nobutnk <nobutnk@gmail.com>
2016-03-06 00:29:29 +0900

    init.sqlを更新した。detail, updated_atを追加

--- tutorial/src/main/resources/sql/init.sql
+++ tutorial/src/main/resources/sql/init.sql
@@ -1,6 +1,14 @@
+create sequence if not exists todo_seq
+increment by 1
+start with 1;
+
 create table if not exists todo(
     todo_id varchar(36) primary key,
     todo_title varchar(30),
+    todo_detail varchar(100),
     finished boolean, 
+    updated_at timestamp,
     created_at timestamp
 );
+
+insert into todo (todo_id, todo_title, todo_detail, finished, updated_at, created_at) values (todo_seq.nextval, 'init_todo', 'init_detail', false, current_timestamp, current_timestamp);
